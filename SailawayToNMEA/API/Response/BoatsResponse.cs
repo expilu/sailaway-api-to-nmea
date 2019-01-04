043c873de934a4fbc10d73fa40b6419c9599f89b
Daniel López Lacalle <expilu@gmail.com>
2019-01-04 11:28:05 +0000

    Adds boats response

---
+++ SailawayToNMEA/API/Response/BoatsResponse.cs
@@ -0,0 +1,16 @@
+﻿using RestSharp.Deserializers;
+using SailawayToNMEA.Model;
+using System;
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+namespace SailawayToNMEA.API.Response
+***REMOVED***
+    class BoatsResponse
+    ***REMOVED***
+        [DeserializeAs(Name = "boats")]
+        public List<BoatInfo> Boats ***REMOVED*** get; set; ***REMOVED***
+***REMOVED***
+***REMOVED***
