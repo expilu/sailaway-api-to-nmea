d404d16c95e4b30f35f33a21ae7564992cffefb7
Daniel López Lacalle <expilu@gmail.com>
2019-01-04 11:27:35 +0000

    Adds boats api method and refresh task

---
+++ SailawayToNMEA/API/Tasks.cs
@@ -0,0 +1,26 @@
+﻿using SailawayToNMEA.App;
+using System;
+***REMOVED***
+***REMOVED***
+***REMOVED***
+using System.Threading;
+***REMOVED***
+
+namespace SailawayToNMEA.API
+***REMOVED***
+    public static class Tasks
+    ***REMOVED***
+        public static async Task RefreshBoats(CancellationToken cancellationToken)
+        ***REMOVED***
+            await Task.Run(async () =>
+            ***REMOVED***
+                while (true)
+                ***REMOVED***
+                    Global.Instance.Boats = Methods.GetBoats();
+                    await Task.Delay(Conf.REQUEST_RATE, cancellationToken);
+                    if (cancellationToken.IsCancellationRequested) break;
+            ***REMOVED***
+        ***REMOVED***);
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
