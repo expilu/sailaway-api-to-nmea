5876e51589ec077f496217f7d2c3e4e0bafd3bc9
Daniel López Lacalle <expilu@gmail.com>
2019-01-04 10:11:15 +0000

    Changes Windows Forms project name

--- SailawayToNMEA/SailawayToNMEA.cs
+++ SailawayToNMEA/SailawayToNMEA.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SailawayToNMEA
 ***REMOVED***
-    public partial class Form1 : Form
+    public partial class SailawayToNMEA : Form
     ***REMOVED***
-        public Form1()
+        public SailawayToNMEA()
         ***REMOVED***
             InitializeComponent();
     ***REMOVED***
