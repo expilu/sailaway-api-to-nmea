ed30d4cd65781129c7b807a5267f4a2407b6af13
Daniel López Lacalle <expilu@gmail.com>
2019-01-04 10:11:15 +0000

    Changes Windows Forms project name

--- SailawayToNMEA/SailawayToNMEA.Designer.cs
+++ SailawayToNMEA/SailawayToNMEA.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SailawayToNMEA
 {
-    partial class Form1
+    partial class SailawayToNMEA
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // SailawayToNMEA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "SailawayToNMEA";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.SailawayToNMEA_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- SailawayToNMEA/SailawayToNMEA.cs
+++ SailawayToNMEA/SailawayToNMEA.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SailawayToNMEA
 {
-    public partial class Form1 : Form
+    public partial class SailawayToNMEA : Form
     {
-        public Form1()
+        public SailawayToNMEA()
         {
             InitializeComponent();
         }
