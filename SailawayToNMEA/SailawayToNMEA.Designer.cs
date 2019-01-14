93c720d59d00913b039c5699c44d73973f8e416d
Daniel López Lacalle <expilu@gmail.com>
2019-01-14 13:26:47 +0000

    Don't allow maximizing

--- SailawayToNMEA/SailawayToNMEA.Designer.cs
+++ SailawayToNMEA/SailawayToNMEA.Designer.cs
@@ -192,6 +192,7 @@ namespace SailawayToNMEA
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "SailawayToNMEA";
             this.Text = "Sailaway to NMEA";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SailawayToNMEA_FormClosing);
